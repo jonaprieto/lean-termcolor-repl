2d4301088f983da6af302261bf26ca3e8e2abbe6
Jonathan Cubides <prieto.jona@gmail.com>
2026-08-08 22:02:19 -0500

    build: update terminal to v0.3.0

--- lakefile.lean
+++ lakefile.lean
@@ -7,7 +7,7 @@ package «termcolor-repl» where
 
 require «termcolor-terminal» from git
   "https://github.com/jonaprieto/lean-termcolor-terminal.git"
-  @ "v0.2.1"
+  @ "v0.3.0"
 
 @[default_target]
 lean_lib «TermColor.Repl» where
