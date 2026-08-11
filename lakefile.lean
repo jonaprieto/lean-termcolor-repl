ed4c9604eec11d048cda983fdaf8ce92dc6fab8f
Jonathan Cubides <prieto.jona@gmail.com>
2026-08-11 11:13:01 -0500

    chore(release): set version 0.8.1

--- lakefile.lean
+++ lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 open Lake DSL
 
 package «termcolor-repl» where
-  version := v!"0.8.0"
+  version := v!"0.8.1"
   leanOptions := #[⟨`autoImplicit, false⟩, ⟨`relaxedAutoImplicit, false⟩]
 
 require «termcolor-terminal» from git
