ee59650e3cb5445ffd3db01536712efcd8599ee3
Jonathan Cubides <prieto.jona@gmail.com>
2026-09-09 14:28:51 -0500

    build: refresh repl dependencies

--- lakefile.lean
+++ lakefile.lean
@@ -7,11 +7,11 @@ package «termcolor-repl» where
 
 require «termcolor-terminal» from git
   "https://github.com/jonaprieto/lean-termcolor-terminal.git"
-  @ "v0.3.7"
+  @ "v0.3.8"
 
 require argus from git
   "https://github.com/jonaprieto/lean-argus.git"
-  @ "v0.5.4"
+  @ "v0.5.5"
 
 @[default_target]
 lean_lib «TermColor.Repl» where
